a5714f7cabfe6583a289c13879b243b00b8289ef
faelz123 <rafael2@outlook.com>
2022-06-30 19:59:21 -0300

    add delete

--- src/components/Cadastro.jsx
+++ src/components/Cadastro.jsx
@@ -6,7 +6,7 @@ import Input from './Input';
 import './Cadastro.css';
 
 const Cadastro = ({handleUfAdd, inputData, setInputData}) => {
-    
+
       const handleUfAddClick = () => {
         console.log(inputData.input1)
         handleUfAdd(inputData);
@@ -20,7 +20,7 @@ const Cadastro = ({handleUfAdd, inputData, setInputData}) => {
         <div className='cadastro-container'>
             <Input inputData={inputData}  setInputData={setInputData} input='input1' label='nome' />
             <Input inputData={inputData}  setInputData={setInputData} input='input2' label='sigla'/>
-            <Input inputData={inputData}  setInputData={setInputData} input='input3' label='código'/>
+            <Input inputData={inputData}  setInputData={setInputData} input='input3' label='status'/>
             
             <div className='input-container'>
                 <Button onClick={handleUfAddClick}>
--- src/components/Editar.jsx
+++ src/components/Editar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import { useParams, useNavigate } from "react-router-dom";
 
 import Input from "./Input";
@@ -11,27 +12,79 @@ const Editar = ({ uf, setUf, inputData, setInputData }) => {
 
   const params = useParams();
   const history = useNavigate();
+  console.log(params);
 
-  const newUf = uf.filter((uf) => (uf.sigla === params.ufselect || uf.codigoUF === parseInt(params.ufselect)));
-  
+  // console.log('editar',uf)
+  // console.log('editar',params)
+
+  const newUf = uf.filter(
+    (uf) =>
+      uf.sigla === params.ufselect || uf.codigoUF === parseInt(params.ufselect)
+  );
+  // console.log(newUf)
+
   const handleBackButton = () => {
     history(`/uf`);
+  };
+
+  // const editedUf = uf.map((uf) => {
+  //   if (uf.codigoUF === ufCod) {
+  //     ufAlterar = {
+  //       codigoUF: ufCod,
+  //       sigla: inputData.input2,
+  //       nome: inputData.input1,
+  //       status: parseInt(inputData.input3)
+  //     }
+  //   }
+  //   return uf;
+  // });
+
+  // if (editedUf[0].codigoUF === ufCod) {
+  //   ufAlterar = {
+  //     codigoUF: ufCod,
+  //     sigla: inputData.input2,
+  //     nome: inputData.input1,
+  //     status: parseInt(inputData.input3)
+  //   }
+  // }
+  const handleEditUf = (ufCod) => {
+    let ufAlterar = null;
+    const editedUf = uf.filter((uf) => uf.codigoUF === ufCod);
+
+    editedUf[0].codigoUF === ufCod &&
+      (ufAlterar = {
+        codigoUF: ufCod,
+        sigla: inputData.input2,
+        nome: inputData.input1,
+        status: parseInt(inputData.input3),
+      });
+    // console.log(editedUf);
+    // console.log(ufAlterar);
+    const alterarUf = async () => {
+      const { data } = await axios.put("http://localhost:3333/uf", ufAlterar);
+      console.log(data);
+      setUf(data.sort().reverse());
+    };
+    alterarUf();
+    history(`/uf`);
+  };
+
+  // const handleUfEdit = (ufCod) => {
+  //   const editedUf = uf.map(uf => {
+  //       console.log(uf)
+  //       if (uf.codigoUF === ufCod){
+  //           return {...uf, nome: inputData.input1, sigla: inputData.input2, codigoUF: parseInt(inputData.input3)};
+  //       }
+  //       return uf;
+  //   })
+  //   setUf(editedUf);
+  //   history(`/uf/${inputData.input2}`)
+  //   setInputData('');
+  // }
+
+  if (!newUf.length) {
+    return <h2>Loading...</h2>;
   }
-
-
-  const handleUfEdit = (ufSigla) => {
-    const editedUf = uf.map(uf => {
-        console.log(uf)
-        if (uf.sigla === ufSigla){
-            return {...uf, nome: inputData.input1, sigla: inputData.input2, codigoUF: parseInt(inputData.input3)};
-        }
-        return uf;
-    })
-    setUf(editedUf);
-    history(`/uf/${inputData.input2}`)
-    setInputData('');
-  }
-
   return (
     <>
       <div className="editar-inputs-container">
@@ -49,12 +102,14 @@ const Editar = ({ uf, setUf, inputData, setInputData }) => {
         />
         <Input
           setInputData={setInputData}
-          label={newUf[0].codigoUF}
-          input='input3'
+          label={newUf[0].status}
+          input="input3"
           inputData={inputData}
         />
         <div>
-          <Button onClick={() => handleUfEdit(params.siglaUF)}>Alterar</Button>
+          <Button onClick={() => handleEditUf(parseInt(params.ufselect))}>
+            Alterar
+          </Button>
         </div>
       </div>
       <Consulta dataUf={newUf} />
